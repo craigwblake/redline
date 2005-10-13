ae0a4d75f5f49816c97a3830499959d8e7425fa1
Craig Blake <craigwblake@gmail.com>
2005-10-13 18:18:36 +0000

    Added new header codes.
    git-svn-id: http://redline-rpm.googlecode.com/svn/redline/trunk@8 da7a95ae-83ad-11de-b471-8552911d2254

--- source/main/org/freecompany/redline/Signature.java
+++ source/main/org/freecompany/redline/Signature.java
@@ -11,14 +11,21 @@ public class Signature extends AbstractHeader {
 	static { for ( SignatureTag tag : SignatureTag.values()) TAGS.put( tag.getCode(), tag); }
 
 	public enum SignatureTag implements Tag {
-		SIGSIZE( 1000, "sigsize"),
-		PGP( 1002, "pgp"),
-		MD5( 1004, "md5"),
-		GPG( 1005, "gpg"),
+		SIGSIZE( 257, "sigsize"),
+		LEGACY_SIGSIZE( 1000, "sigsize"),
+		PGP( 259, "pgp"),
+		LEGACY_PGP( 1002, "pgp"),
+		MD5( 261, "md5"),
+		LEGACY_MD5( 1004, "md5"),
+		GPG( 262, "gpg"),
+		LEGACY_GPG( 1005, "gpg"),
 		PAYLOADSIZE( 1007, "payloadsize"),
-		SHA1HEADER( 1010, "sha1header"),
-		DSAHEADER( 1011, "dsaheader"),
-		RSAHEADER( 1012, "rsaheader");
+		SHA1HEADER( 269, "sha1header"),
+		LEGACY_SHA1HEADER( 1010, "sha1header"),
+		DSAHEADER( 267, "dsaheader"),
+		LEGACY_DSAHEADER( 1011, "dsaheader"),
+		RSAHEADER( 268, "rsaheader"),
+		LEGACY_RSAHEADER( 1012, "rsaheader");
 
 		private int code;
 		private String name;
