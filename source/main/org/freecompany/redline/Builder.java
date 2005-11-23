719ce97925b6d060b93d6cdadfe7db497a3e17da
Craig Blake <craigwblake@gmail.com>
2005-11-23 06:08:08 +0000

    Temporarily disabled SHA headers.
    git-svn-id: http://redline-rpm.googlecode.com/svn/redline/trunk@57 da7a95ae-83ad-11de-b471-8552911d2254

--- source/main/org/freecompany/redline/Builder.java
+++ source/main/org/freecompany/redline/Builder.java
@@ -306,8 +306,8 @@ public class Builder {
 		final Entry< int[]> sigsize = ( Entry< int[]>) format.getSignature().addEntry( LEGACY_SIGSIZE, 1);
 		final Entry< int[]> payload = ( Entry< int[]>) format.getSignature().addEntry( PAYLOADSIZE, 1);
 		final Entry< byte[]> md5 = ( Entry< byte[]>) format.getSignature().addEntry( LEGACY_MD5, 16);
-		final Entry< String[]> sha = ( Entry< String[]>) format.getSignature().addEntry( SHA1HEADER, 1);
-		sha.setSize( SHASIZE);
+		//final Entry< String[]> sha = ( Entry< String[]>) format.getSignature().addEntry( SHA1HEADER, 1);
+		//sha.setSize( SHASIZE);
 
 		format.getLead().write( original);
 		signature.setValues( getSignature( format.getSignature().count()));
@@ -323,12 +323,12 @@ public class Builder {
 		*/
 		
 		final Key< Integer> sigsizekey = output.start();
-		final Key< byte[]> shakey = output.start( "SHA");
+		//final Key< byte[]> shakey = output.start( "SHA");
 		final Key< byte[]> md5key = output.start( "MD5");
 
 		immutable.setValues( getImmutable( format.getHeader().count()));
 		format.getHeader().write( output);
-		sha.setValues( new String[] { Util.hex( output.finish( shakey))});
+		//sha.setValues( new String[] { Util.hex( output.finish( shakey))});
 
 		final GZIPOutputStream zip = new GZIPOutputStream( Channels.newOutputStream( output));
 		final WritableChannelWrapper compressor = new WritableChannelWrapper( Channels.newChannel( zip));
