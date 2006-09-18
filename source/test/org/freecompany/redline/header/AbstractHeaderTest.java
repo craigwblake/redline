906610294bd17850389187fb153f3653df48cc15
Craig Blake <craigwblake@gmail.com>
2006-09-18 20:03:10 +0000

    Fixed tests.
    git-svn-id: http://redline-rpm.googlecode.com/svn/redline/trunk@108 da7a95ae-83ad-11de-b471-8552911d2254

--- source/test/org/freecompany/redline/header/AbstractHeaderTest.java
+++ source/test/org/freecompany/redline/header/AbstractHeaderTest.java
@@ -8,13 +8,14 @@ import static org.freecompany.redline.header.AbstractHeader.*;
 
 public class AbstractHeaderTest extends TestCase {
 
+	@SuppressWarnings( "unchecked")
 	public void testCharSingle() throws Exception {
 		ByteBuffer buffer = ByteBuffer.allocate( 1);
 		buffer.put(( byte) 1);
 		buffer.flip();
 		
 		TestHeader header = new TestHeader();
-		Entry< byte[]> entry = header.createEntry( 1);
+		Entry< byte[]> entry = ( Entry< byte[]>) header.createEntry( 1);
 		entry.setCount( 1);
 		entry.read( buffer);
 		assertEquals( 1, entry.getValues()[ 0]);
@@ -27,6 +28,7 @@ public class AbstractHeaderTest extends TestCase {
 		assertTrue( buffer.equals( data));
 	}
 
+	@SuppressWarnings( "unchecked")
 	public void testCharMultiple() throws Exception {
 		ByteBuffer buffer = ByteBuffer.allocate( 2);
 		buffer.put(( byte) 1);
@@ -34,7 +36,7 @@ public class AbstractHeaderTest extends TestCase {
 		buffer.flip();
 		
 		TestHeader header = new TestHeader();
-		Entry< byte[]> entry = header.createEntry( 1);
+		Entry< byte[]> entry = ( Entry< byte[]>) header.createEntry( 1);
 		entry.setCount( 2);
 		entry.read( buffer);
 		assertEquals( 1, entry.getValues()[ 0]);
@@ -48,13 +50,14 @@ public class AbstractHeaderTest extends TestCase {
 		assertTrue( buffer.equals( data));
 	}
 
+	@SuppressWarnings( "unchecked")
 	public void testInt8Single() throws Exception {
 		ByteBuffer buffer = ByteBuffer.allocate( 1);
 		buffer.put(( byte) 1);
 		buffer.flip();
 		
 		TestHeader header = new TestHeader();
-		Entry< byte[]> entry = header.createEntry( 2);
+		Entry< byte[]> entry = ( Entry< byte[]>) header.createEntry( 2);
 		entry.setCount( 1);
 		entry.read( buffer);
 		assertEquals( 1, entry.getValues()[ 0]);
@@ -67,6 +70,7 @@ public class AbstractHeaderTest extends TestCase {
 		assertTrue( buffer.equals( data));
 	}
 
+	@SuppressWarnings( "unchecked")
 	public void testInt8Multiple() throws Exception {
 		ByteBuffer buffer = ByteBuffer.allocate( 2);
 		buffer.put(( byte) 1);
@@ -74,7 +78,7 @@ public class AbstractHeaderTest extends TestCase {
 		buffer.flip();
 		
 		TestHeader header = new TestHeader();
-		Entry< byte[]> entry = header.createEntry( 2);
+		Entry< byte[]> entry = ( Entry< byte[]>) header.createEntry( 2);
 		entry.setCount( 2);
 		entry.read( buffer);
 		assertEquals( 1, entry.getValues()[ 0]);
@@ -88,13 +92,14 @@ public class AbstractHeaderTest extends TestCase {
 		assertTrue( buffer.equals( data));
 	}
 
+	@SuppressWarnings( "unchecked")
 	public void testInt16Single() throws Exception {
 		ByteBuffer buffer = ByteBuffer.allocate( 2);
 		buffer.putShort(( short) 1);
 		buffer.flip();
 		
 		TestHeader header = new TestHeader();
-		Entry< short[]> entry = header.createEntry( 3);
+		Entry< short[]> entry = ( Entry< short[]>) header.createEntry( 3);
 		entry.setCount( 1);
 		entry.read( buffer);
 		assertEquals( 1, entry.getValues()[ 0]);
@@ -107,6 +112,7 @@ public class AbstractHeaderTest extends TestCase {
 		assertTrue( buffer.equals( data));
 	}
 
+	@SuppressWarnings( "unchecked")
 	public void testInt16Multiple() throws Exception {
 		ByteBuffer buffer = ByteBuffer.allocate( 4);
 		buffer.putShort(( short) 1);
@@ -114,7 +120,7 @@ public class AbstractHeaderTest extends TestCase {
 		buffer.flip();
 		
 		TestHeader header = new TestHeader();
-		Entry< short[]> entry = header.createEntry( 3);
+		Entry< short[]> entry = ( Entry< short[]>) header.createEntry( 3);
 		entry.setCount( 2);
 		entry.read( buffer);
 		assertEquals( 1, entry.getValues()[ 0]);
@@ -128,13 +134,14 @@ public class AbstractHeaderTest extends TestCase {
 		assertTrue( buffer.equals( data));
 	}
 
+	@SuppressWarnings( "unchecked")
 	public void testInt32Single() throws Exception {
 		ByteBuffer buffer = ByteBuffer.allocate( 4);
 		buffer.putInt( 1);
 		buffer.flip();
 		
 		TestHeader header = new TestHeader();
-		Entry< int[]> entry = header.createEntry( 4);
+		Entry< int[]> entry = ( Entry< int[]>) header.createEntry( 4);
 		entry.setCount( 1);
 		entry.read( buffer);
 		assertEquals( 1, entry.getValues()[ 0]);
@@ -147,6 +154,7 @@ public class AbstractHeaderTest extends TestCase {
 		assertTrue( buffer.equals( data));
 	}
 
+	@SuppressWarnings( "unchecked")
 	public void testInt32Multiple() throws Exception {
 		ByteBuffer buffer = ByteBuffer.allocate( 8);
 		buffer.putInt( 1);
@@ -154,7 +162,7 @@ public class AbstractHeaderTest extends TestCase {
 		buffer.flip();
 		
 		TestHeader header = new TestHeader();
-		Entry< int[]> entry = header.createEntry( 4);
+		Entry< int[]> entry = ( Entry< int[]>) header.createEntry( 4);
 		entry.setCount( 2);
 		entry.read( buffer);
 		assertEquals( 1, entry.getValues()[ 0]);
@@ -168,13 +176,14 @@ public class AbstractHeaderTest extends TestCase {
 		assertTrue( buffer.equals( data));
 	}
 
+	@SuppressWarnings( "unchecked")
 	public void testInt64Single() throws Exception {
 		ByteBuffer buffer = ByteBuffer.allocate( 8);
 		buffer.putLong( 1);
 		buffer.flip();
 		
 		TestHeader header = new TestHeader();
-		Entry< long[]> entry = header.createEntry( 5);
+		Entry< long[]> entry = ( Entry< long[]>) header.createEntry( 5);
 		entry.setCount( 1);
 		entry.read( buffer);
 		assertEquals( 1, entry.getValues()[ 0]);
@@ -187,6 +196,7 @@ public class AbstractHeaderTest extends TestCase {
 		assertTrue( buffer.equals( data));
 	}
 
+	@SuppressWarnings( "unchecked")
 	public void testInt64Multiple() throws Exception {
 		ByteBuffer buffer = ByteBuffer.allocate( 16);
 		buffer.putLong( 1);
@@ -194,7 +204,7 @@ public class AbstractHeaderTest extends TestCase {
 		buffer.flip();
 		
 		TestHeader header = new TestHeader();
-		Entry< long[]> entry = header.createEntry( 5);
+		Entry< long[]> entry = ( Entry< long[]>) header.createEntry( 5);
 		entry.setCount( 2);
 		entry.read( buffer);
 		assertEquals( 1, entry.getValues()[ 0]);
@@ -208,13 +218,14 @@ public class AbstractHeaderTest extends TestCase {
 		assertTrue( buffer.equals( data));
 	}
 
+	@SuppressWarnings( "unchecked")
 	public void testStringSingle() throws Exception {
 		ByteBuffer buffer = ByteBuffer.allocate( 8);
 		buffer.put( Charset.forName( "US-ASCII").encode( "1234567\000"));
 		buffer.flip();
 		
 		TestHeader header = new TestHeader();
-		Entry< String[]> entry = header.createEntry( 6);
+		Entry< String[]> entry = ( Entry< String[]>) header.createEntry( 6);
 		entry.setCount( 1);
 		entry.read( buffer);
 		assertEquals( "1234567", entry.getValues()[ 0]);
@@ -227,6 +238,7 @@ public class AbstractHeaderTest extends TestCase {
 		assertTrue( buffer.equals( data));
 	}
 
+	@SuppressWarnings( "unchecked")
 	public void testStringMultiple() throws Exception {
 		ByteBuffer buffer = ByteBuffer.allocate( 16);
 		buffer.put( Charset.forName( "US-ASCII").encode( "1234567\000"));
@@ -234,7 +246,7 @@ public class AbstractHeaderTest extends TestCase {
 		buffer.flip();
 		
 		TestHeader header = new TestHeader();
-		Entry< String[]> entry = header.createEntry( 6);
+		Entry< String[]> entry = ( Entry< String[]>) header.createEntry( 6);
 		entry.setCount( 2);
 		entry.read( buffer);
 		assertEquals( "1234567", entry.getValues()[ 0]);
@@ -248,13 +260,14 @@ public class AbstractHeaderTest extends TestCase {
 		assertTrue( buffer.equals( data));
 	}
 
+	@SuppressWarnings( "unchecked")
 	public void testBinary() throws Exception {
 		ByteBuffer buffer = ByteBuffer.allocate( 8);
 		buffer.put( new String( "12345678").getBytes());
 		buffer.flip();
 		
 		TestHeader header = new TestHeader();
-		Entry< byte[]> entry = header.createEntry( 7);
+		Entry< byte[]> entry = ( Entry< byte[]>) header.createEntry( 7);
 		entry.setCount( 8);
 		entry.read( buffer);
 		assertTrue( ByteBuffer.wrap( "12345678".getBytes()).equals( ByteBuffer.wrap( entry.getValues())));
@@ -267,13 +280,14 @@ public class AbstractHeaderTest extends TestCase {
 		assertTrue( buffer.equals( data));
 	}
 
+	@SuppressWarnings( "unchecked")
 	public void testStringArraySingle() throws Exception {
 		ByteBuffer buffer = ByteBuffer.allocate( 8);
 		buffer.put( Charset.forName( "US-ASCII").encode( "1234567\000"));
 		buffer.flip();
 		
 		TestHeader header = new TestHeader();
-		Entry< String[]> entry = header.createEntry( 8);
+		Entry< String[]> entry = ( Entry< String[]>) header.createEntry( 8);
 		entry.setCount( 1);
 		entry.read( buffer);
 		assertEquals( "1234567", entry.getValues()[ 0]);
@@ -286,6 +300,7 @@ public class AbstractHeaderTest extends TestCase {
 		assertTrue( buffer.equals( data));
 	}
 
+	@SuppressWarnings( "unchecked")
 	public void testStringArrayMultiple() throws Exception {
 		ByteBuffer buffer = ByteBuffer.allocate( 16);
 		buffer.put( Charset.forName( "US-ASCII").encode( "1234567\000"));
@@ -293,7 +308,7 @@ public class AbstractHeaderTest extends TestCase {
 		buffer.flip();
 		
 		TestHeader header = new TestHeader();
-		Entry< String[]> entry = header.createEntry( 8);
+		Entry< String[]> entry = ( Entry< String[]>) header.createEntry( 8);
 		entry.setCount( 2);
 		entry.read( buffer);
 		assertEquals( "1234567", entry.getValues()[ 0]);
@@ -307,13 +322,14 @@ public class AbstractHeaderTest extends TestCase {
 		assertTrue( buffer.equals( data));
 	}
 
+	@SuppressWarnings( "unchecked")
 	public void testI18NStringSingle() throws Exception {
 		ByteBuffer buffer = ByteBuffer.allocate( 8);
 		buffer.put( Charset.forName( "US-ASCII").encode( "1234567\000"));
 		buffer.flip();
 		
 		TestHeader header = new TestHeader();
-		Entry< String[]> entry = header.createEntry( 9);
+		Entry< String[]> entry = ( Entry< String[]>) header.createEntry( 9);
 		entry.setCount( 1);
 		entry.read( buffer);
 		assertEquals( "1234567", entry.getValues()[ 0]);
@@ -326,6 +342,7 @@ public class AbstractHeaderTest extends TestCase {
 		assertTrue( buffer.equals( data));
 	}
 
+	@SuppressWarnings( "unchecked")
 	public void testI18NStringMultiple() throws Exception {
 		ByteBuffer buffer = ByteBuffer.allocate( 16);
 		buffer.put( Charset.forName( "US-ASCII").encode( "1234567\000"));
@@ -333,7 +350,7 @@ public class AbstractHeaderTest extends TestCase {
 		buffer.flip();
 		
 		TestHeader header = new TestHeader();
-		Entry< String[]> entry = header.createEntry( 9);
+		Entry< String[]> entry = ( Entry< String[]>) header.createEntry( 9);
 		entry.setCount( 2);
 		entry.read( buffer);
 		assertEquals( "1234567", entry.getValues()[ 0]);
