f80dd886bfb479f46638090948c6e2608b0ca7a5
Craig Blake <craigwblake@gmail.com>
2006-12-04 02:12:32 +0000

    Updating test class.
    git-svn-id: http://redline-rpm.googlecode.com/svn/redline/trunk@116 da7a95ae-83ad-11de-b471-8552911d2254

--- source/main/org/freecompany/redline/Test.java
+++ source/main/org/freecompany/redline/Test.java
@@ -2,34 +2,34 @@ package org.freecompany.redline;
 
 import java.io.File;
 
-import static org.freecompany.redline.header.RpmType.*;
-import static org.freecompany.redline.header.Architecture.*;
-import static org.freecompany.redline.header.Os.*;
+import static org.freecompany.redline.header.RpmType.BINARY;
+import static org.freecompany.redline.header.Architecture.NOARCH;
+import static org.freecompany.redline.header.Os.LINUX;
 
 public class Test {
 
 	public static void main( String[] args) throws Exception {
 
+		// Set required fields for the RPM package.
 		final Builder builder = new Builder();
 		builder.setPackage( "test", "0.0.1", "1");
 		builder.setType( BINARY);
 		builder.setPlatform( NOARCH, LINUX);
-		builder.setSummary( "A test RPM.");
-		builder.setDescription( "A test RPM with several files.");
+		builder.setSummary( "Test RPM");
+		builder.setDescription( "A test RPM with a few packaged files.");
 		builder.setBuildHost( "localhost");
 		builder.setLicense( "MIT");
 		builder.setGroup( "Miscellaneous");
 		builder.setDistribution( "FreeCompany");
 		builder.setVendor( "FreeCompany RPM Repository http://yum.freecompany.org/");
-		builder.setPackager( "Craig Blake <craigwblake@mac.com>");
+		builder.setPackager( "Jane Doe");
 		builder.setUrl( "http://www.freecompany.org/test/");
 		builder.setProvides( "test");
 
-		for ( int x = 0; x < args.length; x++) {
-			final File file = new File( args[x]);
-			builder.addFile( "/tmp/" + file.getName(), file);
-		}
+		// Adds each file passed as an argument to the package.
+		for ( String file : args) builder.addFile( file, new File( file));
 
+		// This generates a RPM file in the current directory named by the package and type settings.
 		builder.build( new File( "."));
 	}
 }
