84450bb74bceb121a42102771651509dd1488c91
Craig Blake <craigwblake@gmail.com>
2006-12-07 23:45:49 +0000

    Moving Ant task into Redline project.
    git-svn-id: http://redline-rpm.googlecode.com/svn/redline/trunk@124 da7a95ae-83ad-11de-b471-8552911d2254

---
+++ source/main/org/freecompany/redline/ant/Depends.java
@@ -0,0 +1,23 @@
+package org.freecompany.imp.rpm;
+
+public class Depends {
+
+	protected String name;
+	protected String version;
+
+	public void setName( String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setVersion( String version) {
+		this.version = version;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+}
