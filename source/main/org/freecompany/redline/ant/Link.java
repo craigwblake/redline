3557aa5b488e7b5bea432b085942ec9744127731
Craig Blake <craigwblake@gmail.com>
2006-12-07 23:44:06 +0000

    Moving Ant task into Redline project.
    git-svn-id: http://redline-rpm.googlecode.com/svn/redline/trunk@123 da7a95ae-83ad-11de-b471-8552911d2254

---
+++ source/main/org/freecompany/redline/ant/Link.java
@@ -0,0 +1,32 @@
+package org.freecompany.imp.rpm;
+
+public class Link {
+
+	protected String path;
+	protected String target;
+	protected int permissions = -1;
+
+	public void setPath( String path) {
+		this.path = path;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setTarget( String target) {
+		this.target = target;
+	}
+
+	public String getTarget() {
+		return target;
+	}
+
+	public void setPermissions( int permissions) {
+		this.permissions = permissions;
+	}
+
+	public int getPermissions() {
+		return permissions;
+	}
+}
