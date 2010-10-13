e3557e440f03e4f541e6cb960c38bcf4f19d14eb
Craig Blake <craigwblake@mac.com>
2010-10-13 11:44:45 -0400

    Added task in new package.

---
+++ source/main/org/redline-rpm/RedlineTask.java
@@ -0,0 +1,7 @@
+package org.redline_rpm;
+
+/**
+ * Task under new namespace, points to existing task until rest of sources are moved.
+ */
+public class RedlineTask extends org.freecompany.redline.ant.RedlineTask {
+}
