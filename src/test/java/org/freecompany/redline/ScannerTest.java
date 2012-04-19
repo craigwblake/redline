49abab7d453db91e3d04fbc685c5fbd8fc1ea0a7
Karl Heinz Marbaise <info@soebes.de>
2012-04-19 22:17:48 +0200

    - Added next RPM for testing which fails currently with IllegalStateException

--- src/test/java/org/freecompany/redline/ScannerTest.java
+++ src/test/java/org/freecompany/redline/ScannerTest.java
@@ -8,7 +8,12 @@ public class ScannerTest extends TestBase
 {
 
     @Test
-    public void scanTest() throws Exception {
+    public void scanNoArchRPMTest() throws Exception {
         Scanner.main ( new String[]{ getTestResourcesDirectory ( ) + File.separator + "rpm-1-1.0-1.noarch.rpm" } );
+    }
+
+    @Test
+    public void scanSomeArchTest() throws Exception {
+        Scanner.main ( new String[]{ getTestResourcesDirectory ( ) + File.separator + "rpm-3-1.0-1.somearch.rpm" } );
     }
 }
