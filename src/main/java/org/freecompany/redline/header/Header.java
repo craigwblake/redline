675626b596044f5929f22c96f6e2fdc538f87600
Nathan Gutzmann <nathangutzmann@gmail.com>
2013-11-22 13:52:26 -0700

    Fixed storage types for Obsoletes and for Conflicts

--- src/main/java/org/freecompany/redline/header/Header.java
+++ src/main/java/org/freecompany/redline/header/Header.java
@@ -97,13 +97,13 @@ public class Header extends AbstractHeader {
 		REQUIRENAME( 1049, 8, "requirename"),
 		REQUIREVERSION( 1050, 8, "requireversion"),
 		CONFLICTFLAGS( 1053, 6, "conflictflags"),
-		CONFLICTNAME( 1054, 6, "conflictname"),
-		CONFLICTVERSION( 1055, 6, "conflictversion"),
-		OBSOLETENAME( 1090, 6, "obsoletename"),
+		CONFLICTNAME( 1054, 8, "conflictname"),
+		CONFLICTVERSION( 1055, 8, "conflictversion"),
+		OBSOLETENAME( 1090, 8, "obsoletename"),
 		PROVIDEFLAGS( 1112, 4, "provideflags"),
 		PROVIDEVERSION( 1113, 6, "provideversion"),
 		OBSOLETEFLAGS( 1114, 4, "obsoleteflags"),
-		OBSOLETEVERSION( 1115, 6, "obsoleteversion");
+		OBSOLETEVERSION( 1115, 8, "obsoleteversion");
 
 		private int code;
 		private int type;
