d52d1f7fb1a7c46b425af0ae01779ffc99a4ce67
Mark Crossfield <mark.crossfield@autotrader.co.uk>
2014-10-13 14:06:26 +0100

    Testing file modes are correct in Format.toString()

--- src/test/java/org/redline_rpm/ScannerTest.java
+++ src/test/java/org/redline_rpm/ScannerTest.java
@@ -6,8 +6,11 @@ import org.junit.Test;
 import java.io.File;
 import java.io.FileInputStream;
 import java.nio.channels.Channels;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 
 public class ScannerTest extends TestBase
 {
@@ -32,6 +35,17 @@ public class ScannerTest extends TestBase
         Format format = new Scanner().run(channelWrapper(getTestResourcesDirectory ( ) + File.separator + "rpm-1-1.0-1.noarch.rpm"));
         assertEquals(280, format.getHeader().getStartPos());
         assertEquals(4760, format.getHeader().getEndPos());
+    }
+    @Test
+    public void fileModesHeaderIsCorrect() throws Exception {
+        Format format = new Scanner().run(channelWrapper(getTestResourcesDirectory ( ) + File.separator + "rpm-1-1.0-1.noarch.rpm"));
+        String rpmDescription = format.toString();
+        Matcher matcher = Pattern.compile(".*filemodes\\[[^\\]]*\\]\\n[^0-9-]*([^\\n]*).*", Pattern.DOTALL).matcher(rpmDescription);
+        matcher.matches();
+        String [] fileModesFromString = matcher.group(1).split(", ");
+        //String [] actual = {"-32348", "-24065", "16877", "-32275", "-32275", "-32275", "-32275", "-24083", "-32275", "16877", "-32348", "-32348", "16877", "-32348", "-24065,"};
+        String [] expectedFileModes = {"33188", "41471", "16877", "33261", "33261", "33261", "33261", "41453", "33261", "16877", "33188", "33188", "16877", "33188", "41471"};
+        assertArrayEquals(expectedFileModes, fileModesFromString);
     }
 
     private ReadableChannelWrapper channelWrapper(String filename) throws Exception {
