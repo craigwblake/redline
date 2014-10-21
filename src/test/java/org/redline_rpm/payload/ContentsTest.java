cac9f273e8abec4e6f71f9a84165935251641084
michael.hatch <michael.hatch@autotrader.co.uk>
2014-10-21 17:03:44 +0100

    test and implementation of the ability to set the dirmode when adding a file

--- src/test/java/org/redline_rpm/payload/ContentsTest.java
+++ src/test/java/org/redline_rpm/payload/ContentsTest.java
@@ -1,14 +1,21 @@
 package org.redline_rpm.payload;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import junit.framework.TestCase;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class ContentsTest extends TestCase {
 
 	public void testListParents() throws Exception {
 		ArrayList< String> list = new ArrayList< String>();
-		new Contents().listParents( list, new File( "/one/two/three/four"));
+		new Contents().listParents(list, new File("/one/two/three/four"));
 
 		assertEquals( 3, list.size());
 		assertEquals( "/one/two/three", list.get( 0));
@@ -18,7 +25,7 @@ public class ContentsTest extends TestCase {
 
 	public void testListParentsBuiltin() throws Exception {
 		ArrayList< String> list = new ArrayList< String>();
-		new Contents().listParents( list, new File( "/bin/one/two/three/four"));
+		new Contents().listParents(list, new File("/bin/one/two/three/four"));
 
 		assertEquals( 3, list.size());
 		assertEquals( "/bin/one/two/three", list.get( 0));
@@ -48,4 +55,16 @@ public class ContentsTest extends TestCase {
 		assertEquals( "/home/one/two", list.get( 1));
 		assertEquals( "/home/one", list.get( 2));
 	}
+
+    public void testAddFileSetsDirModeOnHeader() throws FileNotFoundException {
+        Contents contents = new Contents();
+        contents.addFile("/test/file.txt", new File("/test/source"), 0777, null, "testuser", "testgroup", 0111);
+        Iterable<CpioHeader> headers = contents.headers();
+        Map<String, Integer> filemodes = new HashMap<String, Integer>();
+        for (CpioHeader header : headers) {
+            filemodes.put(header.getName(), header.getPermissions());
+        }
+        assertThat(filemodes.get("/test"), is(73));
+        assertThat(filemodes.get("/test/file.txt"), is(511));
+    }
 }
